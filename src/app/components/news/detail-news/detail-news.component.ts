3fce2fa1f439781495700da6b7809157a1bf9abf
HDUC122002 <huynhhducc2002@gmail.com>
2025-05-07 08:27:10 +0700

    add view news

---
+++ src/app/components/news/detail-news/detail-news.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../../services/news.service';
+import id from '@angular/common/locales/id';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detail-news',
+  imports: [],
+  templateUrl: './detail-news.component.html',
+  styleUrl: './detail-news.component.scss'
+})
+export class DetailNewsComponent implements OnInit {
+  post: any = {};
+
+  constructor(
+    private newsService: NewsService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadNewsById(id);
+  }
+
+  loadNewsById(id: number){
+    this.newsService.getNewsById(id).subscribe({
+      next: (response) => {
+        debugger
+        this.post = response;
+      },
+      error: (error) => {
+        debugger
+        alert(error.error);
+      }
+    });
+  }
+}

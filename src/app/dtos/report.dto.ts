0fec9efbf758996fcb2392e1ccb2231fbc64b4af
An Pham <vnswann@gmail.com>
2025-05-13 00:02:29 +0700

    update page report

--- src/app/dtos/report.dto.ts
+++ src/app/dtos/report.dto.ts
@@ -7,4 +7,7 @@ export interface ReportDTO {
     captchaToken: string;
     info2?: string; // Tên chủ tài khoản nếu có
     info3?: string; // Ngân hàng nếu có
+
+    // >> Bổ sung: Thêm thuộc tính pageToReport vào đây
+    pageToReport: string; // Hoặc pageToReport?: string; nếu trường này không bắt buộc gửi lên backend
 }
